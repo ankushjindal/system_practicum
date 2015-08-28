45b794417adc4f6454d54099af63371617566ccf
Ankush Jindal <jindal.ankush95@gmail.com>
2015-08-28 11:35:58 +0530

    crr done without rank1

--- lab01/crr.py
+++ lab01/crr.py
@@ -1,14 +1,31 @@
 # for part g
 
-current_point = (1,1)
-min = 1.0
-max = 0.0
+current_point = (1,11)
+minS = 1.0
+minLine = ''
+maxS = 0.0
+maxLine = ''
 
 with open('L4_1000.txt') as infile:
 	for lineNo in infile:
 		temp = lineNo.split('\t')
 		if(current_point==(int(temp[0]),int(temp[1]))):
-			if(float(temp[5])<min):
-				min = float(temp[5])
-			if(float(temp[5])>max):
-				max = float(max[5])
+			if(float(temp[5])<minS):
+				minS = float(temp[5])
+				minLine = temp
+			if(float(temp[5])>maxS):
+				maxS = float(temp[5])
+				maxLine = temp
+		else:
+			crr = float(minLine[5])/float(maxLine[5])
+			# rank1 = 
+			print"{} {} \t [({},{}),{}] \t [({},{}),{}] \t {}".format(current_point[0],current_point[1],minLine[0],minLine[1],minLine[5][:-1],maxLine[0],maxLine[1],maxLine[5][:-1],crr)
+			current_point = (int(temp[0]),int(temp[1]))
+			minS = 1.0
+			minLine = ''
+			maxS = 0.0
+			maxLine = ''
+	#for the last current_point value
+	crr = float(minLine[5])/float(maxLine[5])
+	# rank1 = 
+	print"{} {} \t [({},{}),{}] \t [({},{}),{}] \t {}".format(current_point[0],current_point[1],minLine[0],minLine[1],minLine[5][:-1],maxLine[0],maxLine[1],maxLine[5][:-1],crr)

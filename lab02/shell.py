f88e70c2a23b33b5af01b3e9c0f3ade069a0e9aa
tshrjn <7tjaint@gmail.com>
2015-09-04 02:12:50 +0530

    jdfkjdlkds

--- lab02/shell.py
+++ lab02/shell.py
@@ -6,55 +6,66 @@ def cd(cmd):
 	global pwd
 	temp_pwd = pwd
 	second = cmd[1]
+	
 	if second[0] == '/':
 		pwd = '/'
-		path_list = list(filter(None, second[1:].split('/')))
+		path_list = list(filter(None,second[1:].split('/')))
 	elif second[0] == '~':
 		pwd = os.path.expanduser("~")
 		path_list = list(filter(None,second[1:].split('/')))
 	else:
 		path_list = list(filter(None,second.split('/')))
-	cd_iterative(path_list)
 
-def cd_iterative(path_list):
-	global pwd
+	print(path_list)
 	for path in path_list:
-		temp_pwd = pwd
-		if path == '..':
-			pwd = '/'.join(pwd.split('/')[:-1])
-		else:
-			if(pwd=='/'):
-				pwd += path
-			else:
-				pwd += '/'+path
-		try:
-			if (pwd==''):
-				pwd='/'
-			try_the_directory = os.listdir(pwd)
-		except FileNotFoundError:
-			print('ERROR')
-			pwd = temp_pwd
-			break
+		cd_relative(path)
+
+def cd_relative(path):
+	global pwd
+
+	temp_pwd = pwd
+	if path == '..':
+		pwd = '/'.join(pwd.split('/')[:-1])
+		print(pwd)
+	else:
+		pwd += '/' + path
+	try:
+		try_the_directory = os.listdir(pwd)
+	except FileNotFoundError:
+		print('ERROR')
+		pwd = temp_pwd
 
 def dir(cmd):
-	global pwd
-	try:
-		second = cmd[1]
-		temp_pwd = pwd
-		cd(['cd',second])
-		arr = os.listdir(pwd)
-		print('dir of '+pwd)
-		pwd = temp_pwd
-	except:
-		print('dir of '+pwd)
-		arr = os.listdir(pwd)
+	arr = os.listdir(pwd)
+	print()
 	for x in arr:
 		print(x)
+	print()
+
+def echo(cmd):
+	"""echo <comment>​­ Display <comment>​on the display followed by a new line. 
+	(Multiple spaces/tabs may be reduced to a single space). """
+	comment = ''
+	for x in cmd:
+		if(x != '\t' and x != 'echo'):
+			comment += x + ' '
+	print(comment)
+
+def pause(cmd):
+	"""
+	​Pause Operation of shell until ‘Enter ’ is pressed
+	"""
+	# while(True):
+	input()
+		# if (request[-1] == '\n'):
+			# break
 
 def environ(cmd):
 	arr = os.environ
 	for k in arr:
 		print(k, ": " ,arr[k])
+
+
 
 while True:
 	cmd = input(pwd + '$ ').split()

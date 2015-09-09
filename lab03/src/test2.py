b63fd602bf11798d04afe7322e9ff9b909a527b3
tshrjn <tusharjain.champ@gmail.com>
2015-09-09 05:14:54 -0700

    Question 1 basic

--- lab03/src/test2.py
+++ lab03/src/test2.py
@@ -1,19 +1,52 @@
 from multiprocessing import Process
 import os
+import webbrowser
+import time
+
+file_name  = "abcd.txt"
+sleep_duration = 3
+site = "http://goo.gl/3v6wUh"
 
 def info(title):
     print title
     print 'module name:', __name__
     if hasattr(os, 'getppid'):  # only available on Unix
         print 'parent process:', os.getppid()
-    print 'process id:', os.getpid()
+    print 'process id:', os.getpid() 
 
-def f(name):
-    info('function f')
-    print 'hello', name
+def file_edit(name):
+    info('file_edit')
+    with open(file_name, "ab+") as f:
+        for line in f:
+            line = line.replace(" ","\t")
+
+# def sleep_fn(duration):
+#     time.sleep(duration)
+
+# def open_browser(site):
+#     webbrowser.open(site)
+
+def fork_bomb():
+    info('Fork Bomb')
+    while true:
+        os.fork()
+
+def fork_bomb2():
+    info("Dummy Fork!")
 
 if __name__ == '__main__':
     info('main line')
-    p = Process(target=f, args=('bob',))
-    p.start()
-    p.join()
+    p1 = Process(target=file_edit, args=(file_name,))
+    p2 = Process(target=time.sleep,args=(sleep_duration,))
+    hog1 = Process(target=webbrowser.open,args=(site,))
+    hog2 = Process(target=fork_bomb2)
+    
+    p2.start()
+    hog1.start()    
+    hog2.start()
+    p1.start()
+
+    p1.join()
+    p2.join()
+    hog1.join()
+    hog2.join()
